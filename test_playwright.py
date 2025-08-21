4d92654b125faccaf08618c7c13ec65132328373
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2025-08-21 14:52:51 -0600

    fix: playwright test fix

--- test_playwright.py
+++ test_playwright.py
@@ -10,17 +10,15 @@ load_dotenv()
 # python -m unittest test_playwright.py
 
 class TestSearch(unittest.TestCase):
+
     def setUp(self):
         page = sync_playwright().start().chromium.launch(headless=False).new_page()
         page.goto("https://todomvc.com/examples/vue/dist/#/")
         self.al = Alumni(page)
 
-    def test_add_todo(self):
-        self.al.do("type 'buy some milk' into the input field, then press 'Enter'")
-        self.al.check("todo list contain 'buy some milk'")
-
     def test_complete_all_todos(self):
         self.al.do("type 'buy some milk' into the input field, then press 'Enter'")
         self.al.do("type 'buy some bread' into the input field, then press 'Enter'")
-        self.al.check("buy milk' title font style is strikethrough", vision=True)
-        self.al.check("buy bread' title font style is strikethrough", vision=True)
+        self.al.do("mark all tasks complete")
+        self.al.check("buy some milk' title font style is strikethrough", vision=True)
+        self.al.check("buy some bread' title font style is strikethrough", vision=True)
