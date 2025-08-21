22c14a7e1379e0a98293ec963c34da3fe3c346d4
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2025-08-21 14:43:17 -0600

    feat: test playwright + uniitest

---
+++ test_playwright.py
@@ -0,0 +1,26 @@
+import unittest
+from playwright.sync_api import sync_playwright
+from alumnium import Alumni
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Run with:
+# python -m unittest test_playwright.py
+
+class TestSearch(unittest.TestCase):
+    def setUp(self):
+        page = sync_playwright().start().chromium.launch(headless=False).new_page()
+        page.goto("https://todomvc.com/examples/vue/dist/#/")
+        self.al = Alumni(page)
+
+    def test_add_todo(self):
+        self.al.do("type 'buy some milk' into the input field, then press 'Enter'")
+        self.al.check("todo list contain 'buy some milk'")
+
+    def test_complete_all_todos(self):
+        self.al.do("type 'buy some milk' into the input field, then press 'Enter'")
+        self.al.do("type 'buy some bread' into the input field, then press 'Enter'")
+        self.al.check("buy milk' title font style is strikethrough", vision=True)
+        self.al.check("buy bread' title font style is strikethrough", vision=True)
